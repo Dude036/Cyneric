ca1671bc1faa16bc46dcbe9e4dfdb555ad0283db
Joshua Higham <josh@higham.com>
2021-04-16 04:16:58 -0600

    Adding Admin showcase

--- map/admin.py
+++ map/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
 from .models import Town, Person
 
+
+class TownAdmin(admin.ModelAdmin):
+    list_display = ('name', 'leader', 'governing_body')
+
+
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('name', 'title')
+
+
 # Register your models here.
-admin.site.register(Town)
-admin.site.register(Person)
+admin.site.register(Town, TownAdmin)
+admin.site.register(Person, PersonAdmin)
 
