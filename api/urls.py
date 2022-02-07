25dfbec6bdcaf5f25c50e4096bc95801fc3f7a83
Josh Higham <joshua.higham@collectivemedicaltech.com>
2022-02-07 15:52:04 -0700

    Updating URLs for better usage

--- api/urls.py
+++ api/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('npc/json/', views.npc_json, name='npc_json'),
     path('pc/', views.pc, name='pc'),
     path('pc/json/', views.pc_json, name='pc_json'),
-    path('5e/beast/', views.beast, name='beast'),
-    path('5e/beast/json/', views.beast_json, name='beast_json'),
+    path('beast/5e/', views.beast, name='beast'),
+    path('beast/5e/json/', views.beast_json, name='beast_json'),
 ]
